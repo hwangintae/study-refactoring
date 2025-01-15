bd9bfb531c70f267f7c905fc8a94acd28a371d95
intaeHwang <hwang9029@ckinform.com>
2025-01-15 22:31:02 +0900

    amountFor 함수 생성 및 변수 명 변경 - thisAmount -> result - perf -> aPerformance

--- src/test/java/org/intaehwang/chapter01/StatementTest.java
+++ src/test/java/org/intaehwang/chapter01/StatementTest.java
@@ -33,12 +33,14 @@ class StatementTest {
         String result = statement.getResult();
 
         // then
-        String answer = "청구 내역 (고객명: BigCo)\n" +
-                "  Hamlet: $650.00 (55석)\n" +
-                "  As You Like It: $580.00 (35석)\n" +
-                "  Othello: $500.00 (40석)\n" +
-                "총액: $1,730.00\n" +
-                "적립 포인트: 47점\n";
+        String answer = """
+                청구 내역 (고객명: BigCo)
+                  Hamlet: $650.00 (55석)
+                  As You Like It: $580.00 (35석)
+                  Othello: $500.00 (40석)
+                총액: $1,730.00
+                적립 포인트: 47점
+                """;
 
         assertThat(result).isEqualTo(answer);
     }
