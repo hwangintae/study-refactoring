77f48b672f1ea7d5af43a3f2e9103436c54cd02c
intaeHwang <hwang9029@ckinform.com>
2025-01-17 23:55:41 +0900

    class를 잘 못 분리하여 수정

---
+++ src/main/java/org/intaehwang/chapter01/StatementData.java
@@ -0,0 +1,93 @@
+package org.intaehwang.chapter01;
+
+import lombok.Getter;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+public class StatementData {
+    private final String customer;
+    private final Map<String, Play> plays;
+    private final List<EnrichPerformance> performances;
+    private final int totalAmount;
+    private final int totalVolumeCredits;
+
+    public StatementData (Invoice invoice, Map<String, Play> plays) {
+        this.customer = invoice.customer();
+        this.plays = plays;
+        this.performances = invoice.performances().stream()
+                .map(EnrichPerformance::new)
+                .toList();
+        this.totalAmount = totalAmount(performances);
+        this.totalVolumeCredits = totalVolumeCredits(performances);
+    }
+
+    @Getter
+    public class EnrichPerformance {
+        private final String playId;
+        private final int audience;
+        private final Play play;
+        private final int amount;
+        private final int volumeCredits;
+
+        public EnrichPerformance(Performance aPerformance) {
+            this.playId = aPerformance.playId();
+            this.audience = aPerformance.audience();
+            this.play = playFor(aPerformance);
+            this.amount = amountFor(aPerformance);
+            this.volumeCredits = volumeCreditsFor(aPerformance);
+        }
+    }
+
+    private Play playFor(Performance aPerformance) {
+        return this.plays.get(aPerformance.playId());
+    }
+
+    private int amountFor(Performance aPerformance) {
+        int result = 0;
+        switch (playFor(aPerformance).type()) {
+            case "tragedy": // 비극
+                result = 40_000;
+                if (aPerformance.audience() > 30) {
+                    result += 1_000 * (aPerformance.audience() - 30);
+                }
+                break;
+            case "comedy": // 희극
+                result = 30_000;
+                if (aPerformance.audience() > 20) {
+                    result += 10_000 + 500 * (aPerformance.audience() - 20);
+                }
+                result += 300 * aPerformance.audience();
+                break;
+            default:
+                throw new IllegalArgumentException("알 수 없는 장르: " + playFor(aPerformance).type());
+        }
+
+        return result;
+    }
+
+    private int volumeCreditsFor(Performance aPerformance) {
+        int result = 0;
+        result += Math.max(aPerformance.audience() - 30, 0);
+        if ("comedy".equals(playFor(aPerformance).type())) {
+            result += aPerformance.audience() / 5;
+        }
+
+        return result;
+    }
+
+    private int totalVolumeCredits(List<EnrichPerformance> data) {
+        return data.stream()
+                .map(EnrichPerformance::getVolumeCredits)
+                .reduce(0, Integer::sum);
+    }
+
+    private int totalAmount(List<EnrichPerformance> data) {
+        return data.stream()
+                .map(EnrichPerformance::getAmount)
+                .reduce(0, Integer::sum);
+    }
+
+
+}
