752ced663cbee1b457725205e48cedc091314de6
intaeHwang <hwang9029@ckinform.com>
2025-01-18 01:08:27 +0900

    htmlStatementTest 추가

--- src/test/java/org/intaehwang/chapter01/StatementTest.java
+++ src/test/java/org/intaehwang/chapter01/StatementTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class StatementTest {
 
     @Test
-    public void test1() {
+    public void statementTest() {
         // given
         List<Performance> performances = List.of(
                 new Performance("hamlet", 55),
@@ -27,10 +27,10 @@ class StatementTest {
         plays.put("as-like", new Play("As You Like It", "comedy"));
         plays.put("othello", new Play("Othello", "tragedy"));
 
-        Statement statement = new Statement(invoice, plays);
+        Statement statement = new Statement();
 
         // when
-        String result = statement.statement();
+        String result = statement.statement(invoice, plays);
 
         // then
         String answer = """
@@ -40,6 +40,44 @@ class StatementTest {
                   Othello: $500.00 (40석)
                 총액: $1,730.00
                 적립 포인트: 47점
+                """;
+
+        assertThat(result).isEqualTo(answer);
+    }
+
+    @Test
+    public void htmlStatementTest() {
+        // given
+        List<Performance> performances = List.of(
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40)
+        );
+
+        Invoice invoice = new Invoice("BigCo", performances);
+
+        Map<String, Play> plays = new HashMap<>();
+
+        plays.put("hamlet", new Play("Hamlet", "tragedy"));
+        plays.put("as-like", new Play("As You Like It", "comedy"));
+        plays.put("othello", new Play("Othello", "tragedy"));
+
+        Statement statement = new Statement();
+
+        // when
+        String result = statement.htmlStatement(invoice, plays);
+
+        // then
+        String answer = """
+                <h1>청구 내역 (고객명: BigCo)</h1>
+                <table>
+                <tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>
+                  <tr><td>Hamlet</td><td>(55)</td><td>$650.00</td></tr>
+                  <tr><td>As You Like It</td><td>(35)</td><td>$580.00</td></tr>
+                  <tr><td>Othello</td><td>(40)</td><td>$500.00</td></tr>
+                </table>
+                <p>총액: <em>$1,730.00</em></p>
+                <p>적립 포인트: <em>47</em>점</p>
                 """;
 
         assertThat(result).isEqualTo(answer);
