761037781a8e4f74f65f01699c7052514a2c39da
intaeHwang <hwang9029@ckinform.com>
2025-01-17 23:55:41 +0900

    class를 잘 못 분리하여 수정

--- src/main/java/org/intaehwang/chapter01/Statement.java
+++ src/main/java/org/intaehwang/chapter01/Statement.java
@@ -1,102 +1,32 @@
 package org.intaehwang.chapter01;
 
-import lombok.Getter;
+import org.intaehwang.chapter01.StatementData.EnrichPerformance;
 
-import java.util.List;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Map;
 
 public record Statement(Invoice invoice, Map<String, Play> plays) {
 
     public String statement() {
-        String customer = invoice.customer();
-        List<EnrichPerformance> performances = invoice.performances().stream()
-                .map(EnrichPerformance::new)
-                .toList();
-
-        StatementData statementData = new StatementData(customer, performances);
-
-        return new RenderPlainText(statementData, plays).renderPlainText();
+        return renderPlainText(new StatementData(invoice, plays));
     }
 
-    @Getter
-    public class EnrichPerformance {
-        private final String playId;
-        private final int audience;
-        private final Play play;
-        private final int amount;
-        private final int volumeCredits;
-
-        public EnrichPerformance(Performance aPerformance) {
-            this.playId = aPerformance.playId();
-            this.audience = aPerformance.audience();
-            this.play = playFor(aPerformance);
-            this.amount = amountFor(aPerformance);
-            this.volumeCredits = volumeCreditsFor(aPerformance);
+    private String renderPlainText(StatementData data) {
+        String result = "청구 내역 (고객명: " + data.getCustomer() + ")\n";
+        for (EnrichPerformance perf : data.getPerformances()) {
+            // 청구 내욕을 출력한다.
+            result += "  " + perf.getPlay().name() + ": " + usd(perf.getAmount()) + " (" + perf.getAudience() + "석)\n";
         }
-    }
 
-    private Play playFor(Performance aPerformance) {
-        return plays.get(aPerformance.playId());
-    }
-
-    private int amountFor(Performance aPerformance) {
-        int result = 0;
-        switch (playFor(aPerformance).type()) {
-            case "tragedy": // 비극
-                result = 40_000;
-                if (aPerformance.audience() > 30) {
-                    result += 1_000 * (aPerformance.audience() - 30);
-                }
-                break;
-            case "comedy": // 희극
-                result = 30_000;
-                if (aPerformance.audience() > 20) {
-                    result += 10_000 + 500 * (aPerformance.audience() - 20);
-                }
-                result += 300 * aPerformance.audience();
-                break;
-            default:
-                throw new IllegalArgumentException("알 수 없는 장르: " + playFor(aPerformance).type());
-        }
+        result += "총액: " + usd(data.getTotalAmount()) + "\n";
+        result += "적립 포인트: " + data.getTotalVolumeCredits() + "점\n";
 
         return result;
     }
 
-    private int volumeCreditsFor(Performance aPerformance) {
-        int result = 0;
-        result += Math.max(aPerformance.audience() - 30, 0);
-        if ("comedy".equals(playFor(aPerformance).type())) {
-            result += aPerformance.audience() / 5;
-        }
-
-        return result;
+    private String usd(double aNumber) {
+        return NumberFormat.getCurrencyInstance(Locale.US).format(aNumber / 100.0);
     }
 
-    private int totalVolumeCredits(List<EnrichPerformance> data) {
-        return data.stream()
-                .map(EnrichPerformance::getVolumeCredits)
-                .reduce(0, Integer::sum);
-    }
-
-    private int totalAmount(List<EnrichPerformance> data) {
-        return data.stream()
-                .map(EnrichPerformance::getAmount)
-                .reduce(0, Integer::sum);
-    }
-
-
-    @Getter
-    public class StatementData {
-        private final String customer;
-        private final List<EnrichPerformance> performances;
-        private final int totalAmount;
-        private final int totalVolumeCredits;
-
-        public StatementData(String customer, List<EnrichPerformance> performances) {
-            this.customer = customer;
-            this.performances = performances;
-            this.totalAmount = totalAmount(performances);
-            this.totalVolumeCredits = totalVolumeCredits(performances);
-        }
-    }
 }
