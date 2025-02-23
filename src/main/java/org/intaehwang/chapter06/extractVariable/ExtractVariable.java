ea58ffcd38d28e0baa9367042957f6a26b51e0c2
intaeHwang <hwang9029@ckinform.com>
2025-02-23 17:41:05 +0900

    refactor/ 변수 추출하기 적용

--- src/main/java/org/intaehwang/chapter06/extractVariable/ExtractVariable.java
+++ src/main/java/org/intaehwang/chapter06/extractVariable/ExtractVariable.java
@@ -4,8 +4,10 @@ import org.intaehwang.chapter06.comm.Order;
 
 public class ExtractVariable {
     public double price(Order order) {
-        return order.getQuantity() * order.getItemPrice() -
-                Math.max(0, order.getQuantity() - 500) * order.getItemPrice() * 0.05 +
-                Math.min(order.getQuantity() * order.getItemPrice() * 0.1, 100);
+        double basePrice = order.getQuantity() * order.getItemPrice();
+        double quantityDiscount = Math.max(0, order.getQuantity() - 500) * order.getItemPrice() * 0.05;
+        double shipping = Math.min(basePrice * 0.1, 100);
+
+        return basePrice - quantityDiscount + shipping;
     }
 }
