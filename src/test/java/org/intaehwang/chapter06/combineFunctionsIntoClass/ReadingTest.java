d02b146d9f0c6b8ea801684fccf45faa16e1c184
intaeHwang <hwang9029@ckinform.com>
2025-02-25 23:10:58 +0900

    refactor/ baseCharge 인라인

--- src/test/java/org/intaehwang/chapter06/combineFunctionsIntoClass/ReadingTest.java
+++ src/test/java/org/intaehwang/chapter06/combineFunctionsIntoClass/ReadingTest.java
@@ -13,7 +13,7 @@ class ReadingTest {
         Reading aReading = new Reading("ivan", 10, 5, 2017);
 
         // when
-        int baseCharge = baseRate(aReading.month(), aReading.year()) * aReading.quantity();
+        int baseCharge = aReading.baseCharge(aReading);
 
         assertThat(baseCharge).isEqualTo(100);
     }
@@ -22,10 +22,9 @@ class ReadingTest {
     public void client2() {
         // given
         Reading aReading = new Reading("ivan", 10, 5, 2017);
-        int base = (baseRate(aReading.month(), aReading.year()) * aReading.quantity());
 
         // when
-        int taxableCharge = Math.max(0, base - taxThreshold(aReading.year()));
+        int taxableCharge = Math.max(0, aReading.baseCharge(aReading) - taxThreshold(aReading.year()));
 
         // then
         assertThat(taxableCharge).isEqualTo(60);
@@ -41,15 +40,6 @@ class ReadingTest {
 
         // then
         assertThat(basicChargeAmount).isEqualTo(100);
-    }
-
-    public int baseRate(int month, int year) {
-        if (month >= 6 && month <= 8) { // 여름철
-            return 12;
-        } else if (month == 12 || month == 1 || month == 2) { // 겨울철
-            return 15;
-        }
-        return 10; // 기본 요금
     }
 
     public int taxThreshold(int month) {
