9c5ab53054e0f70257b663f8f0b1e8e1450989f8
intaeHwang <hwang9029@ckinform.com>
2025-02-26 23:55:28 +0900

    refactor/ fileName을 CommandLine으로 이동

--- src/main/java/org/intaehwang/chapter06/splitPhase/JavaExampleCode.java
+++ src/main/java/org/intaehwang/chapter06/splitPhase/JavaExampleCode.java
@@ -18,15 +18,14 @@ public class JavaExampleCode {
         if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
         CommandLine commandLine = new CommandLine();
         commandLine.onlyCountReady = Stream.of(args).anyMatch(arg -> "-r".equals(arg));
+        commandLine.fileName = args[args.length - 1];
 
-        String fileName = args[args.length - 1];
 
-
-        return countOrders(commandLine, args, fileName);
+        return countOrders(commandLine, args);
     }
 
-    private static long countOrders(CommandLine commandLine, String[] args, String fileName) throws IOException {
-        File input = Paths.get(fileName).toFile();
+    private static long countOrders(CommandLine commandLine, String[] args) throws IOException {
+        File input = Paths.get(commandLine.fileName).toFile();
         ObjectMapper mapper = new ObjectMapper();
         Order[] orders = mapper.readValue(input, Order[].class);
 
@@ -39,5 +38,6 @@ public class JavaExampleCode {
 
     private static  class CommandLine {
         boolean onlyCountReady;
+        String fileName;
     }
 }
