1e36fda0c5ceafeb7105585eee5459b429a6876c
intaeHwang <hwang9029@ckinform.com>
2025-02-26 22:09:47 +0900

    refactor/ 두 단계로 나누기

--- src/main/java/org/intaehwang/chapter06/splitPhase/SplitPhase.java
+++ src/main/java/org/intaehwang/chapter06/splitPhase/SplitPhase.java
@@ -5,11 +5,16 @@ public class SplitPhase {
         double basePrice = product.basePrice() * quantity;
         double discount = Math.max(quantity - product.discountThreshold(), 0)
                 * product.basePrice() * product.discountRate();
+        double price = applyShipping(basePrice, shippingMethod, quantity, discount);
+
+        return price;
+    }
+
+    public static double applyShipping(double basePrice, ShippingMethod shippingMethod, int quantity, double discount) {
         double shippingPerCase = (basePrice > shippingMethod.discountThreshold())
                 ? shippingMethod.discountFee() : shippingMethod.feePerCase();
         double shippingCost = quantity * shippingPerCase;
-        double price = basePrice - discount + shippingCost;
 
-        return price;
+        return basePrice - discount + shippingCost;
     }
 }
