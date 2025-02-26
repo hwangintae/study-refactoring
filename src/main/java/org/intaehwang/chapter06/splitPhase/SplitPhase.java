c800e9ce5412bd814b0b236d45f11d8acb556675
intaeHwang <hwang9029@ckinform.com>
2025-02-26 22:18:13 +0900

    refactor/ 중간 데이터 구조 추가

--- src/main/java/org/intaehwang/chapter06/splitPhase/SplitPhase.java
+++ src/main/java/org/intaehwang/chapter06/splitPhase/SplitPhase.java
@@ -5,16 +5,18 @@ public class SplitPhase {
         double basePrice = product.basePrice() * quantity;
         double discount = Math.max(quantity - product.discountThreshold(), 0)
                 * product.basePrice() * product.discountRate();
-        double price = applyShipping(basePrice, shippingMethod, quantity, discount);
+
+        PriceDate priceDate = new PriceDate(basePrice, quantity, discount);
+        double price = applyShipping(priceDate , shippingMethod);
 
         return price;
     }
 
-    public static double applyShipping(double basePrice, ShippingMethod shippingMethod, int quantity, double discount) {
-        double shippingPerCase = (basePrice > shippingMethod.discountThreshold())
+    public static double applyShipping(PriceDate priceDate, ShippingMethod shippingMethod) {
+        double shippingPerCase = (priceDate.basePrice() > shippingMethod.discountThreshold())
                 ? shippingMethod.discountFee() : shippingMethod.feePerCase();
-        double shippingCost = quantity * shippingPerCase;
+        double shippingCost = priceDate.quantity() * shippingPerCase;
 
-        return basePrice - discount + shippingCost;
+        return priceDate.basePrice() - priceDate.discount() + shippingCost;
     }
 }
