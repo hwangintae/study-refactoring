55d166d7221915d60791181992ef7da68a8e29b1
intaeHwang <hwang9029@ckinform.com>
2025-02-26 23:17:38 +0900

    refactor/ run 메서드 추출

--- src/main/java/org/intaehwang/chapter06/splitPhase/JavaExampleCode.java
+++ src/main/java/org/intaehwang/chapter06/splitPhase/JavaExampleCode.java
@@ -3,22 +3,27 @@ package org.intaehwang.chapter06.splitPhase;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class JavaExampleCode {
     public static void main(String[] args) {
         try {
-            if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
-            String fileName = args[args.length - 1];
-            File input = Paths.get(fileName).toFile();
-            ObjectMapper mapper = new ObjectMapper();
-            Order[] orders = mapper.readValue(input, Order[].class);
-            if (Stream.of(args).anyMatch(arg -> "-r".equals(arg))) {
-                System.out.println(Stream.of(orders)
-                        .filter(o -> "ready".equals(o.status()))
-                        .count());
-            } else System.out.println(orders.length);
+            System.out.println(run(args));
         } catch(Exception ignore) {}
+    }
+
+    public static long run(String[] args) throws IOException {
+        if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
+        String fileName = args[args.length - 1];
+        File input = Paths.get(fileName).toFile();
+        ObjectMapper mapper = new ObjectMapper();
+        Order[] orders = mapper.readValue(input, Order[].class);
+        if (Stream.of(args).anyMatch(arg -> "-r".equals(arg))) {
+            return Stream.of(orders)
+                    .filter(o -> "ready".equals(o.status()))
+                    .count();
+        } else return orders.length;
     }
 }
