9729724a8ecff3c8f94d373596d78cde04c41881
intaeHwang <hwang9029@ckinform.com>
2025-03-06 12:28:56 +0900

    feat/ 위임 숨기기

---
+++ src/main/java/org/intaehwang/chapter07/hideDelegate/Department.java
@@ -0,0 +1,11 @@
+package org.intaehwang.chapter07.hideDelegate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Department {
+    private String changeCode;
+    private String manager;
+}
