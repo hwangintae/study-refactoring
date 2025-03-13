9544daab7143dc548f3785fdf57101faa168b42a
intaeHwang <hwang9029@ckinform.com>
2025-03-13 19:10:07 +0900

    feat / 반복문을 파이프라인으로 바꾸기

--- src/test/java/org/intaehwang/chapter08/splitLoop/PeopleTest.java
+++ src/test/java/org/intaehwang/chapter08/splitLoop/PeopleTest.java
@@ -1,10 +1,13 @@
 package org.intaehwang.chapter08.splitLoop;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 class PeopleTest {
 
@@ -45,6 +48,43 @@ class PeopleTest {
                 .map(People::getAge)
                 .min(Integer::compareTo)
                 .orElseThrow(() -> new IllegalStateException("empty people"));
+    }
+
+    @Test
+    public void replaceLoopWithPipeline() {
+        // given
+        String input = """
+                office, country, telephone
+                Chicago, USA, +1 312 373 1000
+                Beijing, China, +86 4008 900 505
+                Bangalore, India, +91 80 4064 9570
+                Porto Alegre, Brazil, +55 51 3079 3550
+                Chennai, India, +91 44 660 44766
+                """;
+
+        // when
+        List<Map<String, String>> result = acquireData(input);
+
+        // then
+        assertThat(result)
+                .containsExactly(
+                        Map.of("city", "Bangalore", "phone", "+91 80 4064 9570"),
+                        Map.of("city", "Chennai", "phone", "+91 44 660 44766")
+                );
+
+    }
+
+    List<Map<String, String>> acquireData(String input) {
+        String[] lines = input.split("\n");
+
+        return Arrays.stream(lines)
+                .skip(1)
+                .filter(line -> !line.isBlank())
+                .map(line -> line.split(","))
+                .filter(record -> record[1].trim().equals("India"))
+                .map(record -> Map.of("city", record[0].trim(),
+                        "phone", record[2].trim()))
+                .toList();
     }
 
 }
